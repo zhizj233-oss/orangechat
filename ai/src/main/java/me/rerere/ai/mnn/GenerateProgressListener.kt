1c2956249903a55282e63f2e0a493656eb386626
re-ovo <1609403959@qq.com>
2025-12-17 11:54:38 +0800

    chore: 移除无用的MNN依赖

--- ai/src/main/java/me/rerere/ai/mnn/GenerateProgressListener.kt
+++
@@ -1,8 +0,0 @@
-// Created by ruoyi.sjd on 2025/5/7.
-// Copyright (c) 2024 Alibaba Group Holding Limited All rights reserved.
-
-package me.rerere.ai.mnn
-
-interface GenerateProgressListener {
-    fun onProgress(progress: String?): Boolean
-}
